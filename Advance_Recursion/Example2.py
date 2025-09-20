cc356e6a6de8b740e9d577d22bbbda319f56f914
MuhammadMudassarYounas <151513066+MuhammadMudassarYounas@users.noreply.github.com>
2025-09-20 08:50:53 +0500

    Example 2

---
+++ Advance_Recursion/Example2.py
@@ -0,0 +1,15 @@
+#Example 2: All Subsequence leet code
+num=[5,9,7]
+def AllSequence(nums):
+    Result=[]
+    def solve(index,subSet):
+        if index>=len(nums):
+            Result.append(subSet.copy())
+            return
+        subSet.append(nums[index])
+        solve(index+1,subSet)
+        subSet.pop()
+        solve(index+1,subSet)
+    solve(0,[])
+    return Result
+print(AllSequence(num))
